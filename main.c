dc3820d56455bdddd549a04f026b80bfac97ea44
Agustin_Vazquez <aguvazquez@itba.edu.ar>
2020-05-13 19:48:31 -0300

    El main ya fue corregido , compila sin warnings y funcionagit status!

--- main.c
+++ main.c
@@ -5,19 +5,14 @@ int suma_digitos(int);
 int main(void) {
     int result;
     result=suma_digitos(1234);
-    printf("%d",result);
+    printf("%d",result);      //Resultado = 10
     return 0;
 }
 int suma_digitos( int n1){
-  
-        return((n1%10)+digitos(n1/10)); //caso recursivo
-}   
-/*a) El programa está enfocado a sumar los digitos de un número 
- * entero.
- * b)El programa no funciona por dos razones.
- *         1)En la función "suma_digitos", mas precisamente en
- *     el caso recursivo , se llama a una funcion que no está definida
- *     "digitos", por ende el programa ni si quiera compila.
- *          2)Dicha funcion le falta el caso base , pues corrigiendo 
- *     el error 1 , el programa nunca saldria de la funcion "suma_digitos",
- *     por ende , no funcionaria.
+    if((n1%10)==0){	//Caso base.
+	return 0;
+    }
+    else{
+        return((n1%10)+suma_digitos(n1/10)); //caso recursivo.
+    }   
+}
