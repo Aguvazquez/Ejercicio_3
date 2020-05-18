ea1c0fc35190423055f7c796bbf32d754f91ac9b
Tiko <frmendizabal@itba.edu.ar>
2020-05-17 21:11:26 -0300

    El programa compila sin warnings y funciona correctamente, solucionando el problema de que no se tuvo en cuenta el caso base, que existiera un 0 intermedio.

--- main.c
+++ main.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 
 int suma_digitos(int);
-int main(void) {
+int main(void) 
+{
     int result;
-    result=suma_digitos(1234);
-    printf("%d",result);      //Resultado = 10
+    result=suma_digitos(10203004);
+    printf("%d\n",result);      //Resultado = 10
     return 0;
 }
-int suma_digitos( int n1){
-    if((n1%10)==0){	//Caso base.
+
+int suma_digitos(int n1)
+{
+    if((n1%10)==0 && n1<10)	//Caso base.
+    {	
 	return 0;
     }
-    else{
+    else
+    {
         return((n1%10)+suma_digitos(n1/10)); //caso recursivo.
     }   
 }
